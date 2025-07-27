418b0ed0301effb9b4411e35f4fea5e4d096dbb4
nbintang <nbintang@gmail.com>
2025-07-27 11:41:12 +0700

    retry: cors

--- src/main.ts
+++ src/main.ts
@@ -8,13 +8,12 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api');
-  app.enableCors({
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    credentials: true,
-  });
   app.use(cookieParser());
   app.use(compression());
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
