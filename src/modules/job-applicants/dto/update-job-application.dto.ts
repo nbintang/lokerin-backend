edbc286104477a312adce516c6dc518ad8089ee3
nbintang <nbintangh@gmail.com>
2025-08-06 17:56:09 +0700

    fix: failed uuid parse

--- src/modules/job-applicants/dto/update-job-application.dto.ts
+++ src/modules/job-applicants/dto/update-job-application.dto.ts
@@ -1,7 +1,7 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateJobApplicationStatusDto } from './create-job-application.dto';
 import { JobApplicationStatus } from '../enum/job-application.enum';
-import { IsArray, IsEnum, IsUUID } from 'class-validator';
+import { ArrayNotEmpty, IsArray, IsEnum, IsUUID } from 'class-validator';
 
 export class UpdateJobApplicationDto extends PartialType(
   CreateJobApplicationStatusDto,
@@ -10,6 +10,7 @@ export class UpdateJobApplicationDto extends PartialType(
   status: JobApplicationStatus;
 
   @IsArray()
-  @IsUUID('all', { each: true })
+  @ArrayNotEmpty()
+  @IsUUID('4', { each: true })
   applicantIds: string[];
 }
